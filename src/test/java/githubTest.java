246836509a163d81f08c734dc26c3a2067378896
Администратор <maksimdemsky@gmail.com>
2021-03-18 23:11:10 +0300

    push complete test

---
+++ src/test/java/githubTest.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class githubTest {
+
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void SearchSelenide() {
+        open("https://github.com/");
+        $("[name=q]").click();
+        $("[name=q]").setValue("selenide").pressEnter();
+        $(".repo-list").find(byText("selenide/")).click();
+        $(".js-repo-nav").find(byText("Wiki")).click();
+        $(".wiki-rightbar").find(byText("SoftAssertions")).click();
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class"));
+
+
+    }
+}
